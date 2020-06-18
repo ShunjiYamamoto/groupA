18cc094d837774ba5f6b597a0e1c743f848a7163
ikehara5155 <20ikehara_koichi@axiz.co.jp>
2020-06-18 15:01:14 +0900

    アカウント

---
+++ プログラム/アカウント/Sutebo_groupA/src/main/java/jp/co/example/controller/IndexController.java
@@ -0,0 +1,25 @@
+package jp.co.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping("/loginPage")
+    public String login(Model model) {
+        return "loginPage";
+    }
+
+    @RequestMapping("/createAccount")
+    public String createAccount(Model model) {
+        return "createAccount";
+    }
+
+}
