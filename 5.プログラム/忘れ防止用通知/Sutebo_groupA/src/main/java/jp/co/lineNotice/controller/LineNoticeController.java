590842696a25724c385decf63571bf899782d7d4
ikehara5155 <20ikehara_koichi@axiz.co.jp>
2020-06-24 18:40:52 +0900

    池原担当

--- 5.プログラム/忘れ防止用通知/Sutebo_groupA/src/main/java/jp/co/lineNotice/controller/LineNoticeController.java
+++ 5.プログラム/忘れ防止用通知/Sutebo_groupA/src/main/java/jp/co/lineNotice/controller/LineNoticeController.java
@@ -50,8 +50,8 @@ public class LineNoticeController {
 	public String lineCheck(@ModelAttribute("test") LineNoticeForm form, Model model) {
 
 		String userId = (String) session.getAttribute("userId");  // 取得
-		boolean check = false;
 
+		//日付フォーマットの設定
 		SimpleDateFormat sdf2 = new SimpleDateFormat("HH");
 		SimpleDateFormat sdf3 = new SimpleDateFormat("mm");
 
@@ -59,41 +59,36 @@ public class LineNoticeController {
 
 		if(ParamUtil.isNullOrEmpty(form.getLineToken()) == true){
 			model.addAttribute("msg_lineToken", "トークンが入力されていません");
-			check = true;
-		}
-		if(ParamUtil.isNullOrEmptyNumber(form.getHour()) == true){
+
+		}else if(ParamUtil.isNullOrEmptyNumber(form.getHour()) == true){
 			model.addAttribute("msg_lineTime", "時間が入力されていません");
-			check = true;
 
 		}else if(ParamUtil.isNullOrEmptyNumber(form.getMinute()) == true){
 			model.addAttribute("msg_lineTime", "時間が入力されていません");
-			check = true;
 
 		}else if(form.getHour() < 0 || form.getHour() >= 24 ) {
 			model.addAttribute("msg_lineTime", "正しい数値が入力されていません");
-			check = true;
 
 		}else if(form.getMinute() < 0 || form.getMinute() >= 60) {
 			model.addAttribute("msg_lineTime", "正しい数値が入力されていません");
-			check = true;
 
-		}else if(form.getHour() < Integer.parseInt(sdf2.format(date))
-				|| form.getMinute() <= Integer.parseInt(sdf3.format(date))) {
+		}else {
 
-			//日付フォーマットの設定
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			//インスタンス作成
 			Calendar calendar = Calendar.getInstance();
-
 			//日付を+1してセット
 			calendar.add(Calendar.DAY_OF_MONTH,1);
-			date = calendar.getTime();
 
-		}
+			if(form.getHour() < Integer.parseInt(sdf2.format(date))) {
+				date = calendar.getTime();
 
-		if(check ==false) {
+			}else if(form.getHour() == Integer.parseInt(sdf2.format(date))
+					&& form.getMinute() <= Integer.parseInt(sdf3.format(date))) {
+
+				date = calendar.getTime();
+			}
+
 			int a = UserService.update(userId,form.getLineToken(),form.getHour(),form.getMinute(),form.getLineNoticeOn(),date);
-
 		}
 
 		return "lineNotice";
