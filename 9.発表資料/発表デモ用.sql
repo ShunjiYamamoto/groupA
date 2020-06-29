24f1e78dc0694726a8db9a84266354c4faa77801
axizKinjo <20kinjo_wataru@axiz.co.jp>
2020-06-29 10:52:30 +0900

    デモ用のSQL

--- 9.発表資料/発表デモ用.sql
+++ 9.発表資料/発表デモ用.sql
@@ -8,3 +8,4 @@ INSERT INTO notices_month_setting (users_id, notice_day, content) VALUES
 --通知のリセット、ログイン日の調整（デモを行う前に毎回実行する）
 DELETE FROM notices_month;
 UPDATE users SET last_login_date = '2020-5-1' WHERE user_id = 'kinjo';
+
